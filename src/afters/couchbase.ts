9de9618e5c59e853e079508d1b6ca256cbfa663d
cmuhoza <ceddymuhoza@gmail.com>
2021-09-19 17:26:41 -0400

    roadman version 1 completed

--- src/afters/couchbase.ts
+++ src/afters/couchbase.ts
@@ -4,25 +4,25 @@ import chalk from 'chalk';
 import {PORT, log} from '../config';
 import {startCouchbase} from '../database';
 import {RoadmanBuild} from '../shared';
+import awaitTo from '../utils/awaitTo';
 
 /**
  * An example of a last RoadMan
  * @param RoadmanBuild
  */
 export const couchbaseRoadman = async ({httpServer, pubsub}: RoadmanBuild): Promise<void> => {
-    startCouchbase()
-        .then(() => {
-            httpServer.listen(Number(PORT), () => {
-                log(chalk.green(`Server started on port ${PORT}, Redis=${pubsub ? true : false}`));
-            });
-        })
-        .catch((error) => {
-            log(
-                chalk.red(
-                    `Error starting Couchbase on port ${PORT}, Redis=${pubsub ? true : false}`
-                )
-            );
-            console.error(error);
-            process.exit(1);
+    const [errorStarting, started] = await awaitTo(startCouchbase());
+
+    if (started) {
+        log(chalk.green(`Couchbase started`));
+        httpServer.listen(Number(PORT), () => {
+            log(chalk.green(`Server started on port ${PORT}, Redis=${pubsub ? true : false}`));
         });
+    }
+
+    if (errorStarting) {
+        log(chalk.red(`Error starting Couchbase on port ${PORT}, Redis=${pubsub ? true : false}`));
+        console.error(errorStarting);
+        process.exit(1);
+    }
 };
